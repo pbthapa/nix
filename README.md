04ad50b2147e53cb10484b2cc4be4f51f3229aef
pbthapa <ptpbthapa24@gmail.com>
2024-06-01 21:16:12 +0200

    home.nix updated

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ nix run . -- build  --flake .
 
 ln -s YOUR_HOME-MANAGER_FOLDER ~/.config
 
-home-manager switch
+home-manager switch --flake .
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,9 +2,10 @@
   home.username = "pushpathapa";
   home.homeDirectory = "/Users/pushpathapa";
   home.stateVersion = "23.11";
-  home.packages = [
-    pkgs.nixpkgs-fmt
-    pkgs.cowsay
+  home.packages = with pkgs; [
+    nixpkgs-fmt
+    cowsay
+    hello
   ];
   programs.home-manager.enable = true;
 }
