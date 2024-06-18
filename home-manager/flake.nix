5328d395b350730dcb727701abb5130c10246837
pbthapa <ptpbthapa24@gmail.com>
2024-06-18 03:11:20 +0200

    flake with more environments

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,27 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      defaultPackage.${system} = home-manager.defaultPackage.${system};
-      homeConfigurations."pushpathapa" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = { nixpkgs, home-manager, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        user = "pushpathapa";
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        #defaultPackage = home-manager.defaultPackage.${system};
+        packages.homeConfigurations."${user}-${system}" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        modules = [
-          ./home.nix
-        ];
+          modules = [
+            ./home.nix
+          ];
 
-        extraSpecialArgs = { inherit inputs; };
-      };
-    };
+          extraSpecialArgs = { inherit inputs; };
+        };
+      });
 }
