36ff0538b6e947386ffb1cd2773c9f15d26209e6
pbthapa <ptpbthapa24@gmail.com>
2024-07-28 13:06:32 +0200

    catppuccin only theme

--- lazyvim/lua/plugins/theme_selector.lua
+++ lazyvim/lua/plugins/theme_selector.lua
@@ -1,18 +1,4 @@
-local chooseColorScheme = function()
-  local hour = os.date("*t").hour
-  local colorScheme = "gruvbox"
-  if hour >= 13 and hour < 24 then
-    colorScheme = "catppuccin"
-  else
-    colorScheme = "gruvbox"
-  end
-  return colorScheme
-end
-
 return {
-  { "ellisonleao/gruvbox.nvim", name = "gruvbox", lazy = true, opts = {
-    transparent_mode = true,
-  } },
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -23,7 +9,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = chooseColorScheme(),
+      colorscheme = "catppuccin",
     },
   },
 }
