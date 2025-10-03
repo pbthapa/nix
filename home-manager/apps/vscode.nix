7c1ff707d4a9d610c7e81340f4a58186b4b4c300
pbthapa <ptpbthapa24@gmail.com>
2025-10-03 08:32:16 +0200

    update deprecrated fields

--- home-manager/apps/vscode.nix
+++ home-manager/apps/vscode.nix
@@ -9,41 +9,43 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
-    enableUpdateCheck = true;
-    enableExtensionUpdateCheck = true;
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      vscjava.vscode-spring-initializr
-      vscjava.vscode-maven
-      #Themes
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
-      #Code formatter
-      esbenp.prettier-vscode
-    ];
-    userSettings = {
-      "editor.fontSize" = 16;
-      "editor.formatOnSave" = true;
-      "javascript.editor.defaultFormatter" = "esbenp.prettier-vscode";
-      "editor.semanticHighlighting.enabled" = true;
-      "terminal.integrated.minimumContrastRatio" = 1;
-      "files.encoding" = "utf8";
-      "files.eol" = "\n";
-      "files.exclude" = {
-        "**/node_modules/**" = true;
-        "**/.git" = true;
-        "**/.DS_Store" = true;
-        "**/Thumbs.db" = true;
+    profiles.default = {
+      enableUpdateCheck = true;
+      enableExtensionUpdateCheck = true;
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+        vscjava.vscode-spring-initializr
+        vscjava.vscode-maven
+        #Themes
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        #Code formatter
+        esbenp.prettier-vscode
+      ];
+      userSettings = {
+        "editor.fontSize" = 16;
+        "editor.formatOnSave" = true;
+        "javascript.editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "editor.semanticHighlighting.enabled" = true;
+        "terminal.integrated.minimumContrastRatio" = 1;
+        "files.encoding" = "utf8";
+        "files.eol" = "\n";
+        "files.exclude" = {
+          "**/node_modules/**" = true;
+          "**/.git" = true;
+          "**/.DS_Store" = true;
+          "**/Thumbs.db" = true;
+        };
+        "editor.tabSize" = 2;
+        "workbench.startupEditor" = "none";
+        "workbench.iconTheme" = "catppuccin-mocha";
+        "workbench.colorTheme" = "Catppuccin Mocha";
+        "catppuccin.accentColor" = "pink";
+        "terminal.external.osxExec" = "Alacritty.app";
+        "terminal.integrated.shell.osx" = "/bin/zsh";
+        "terminal.integrated.fontFamily" = "MesloLGS NF";
+        "terminal.integrated.fontSize" = 16;
       };
-      "editor.tabSize" = 2;
-      "workbench.startupEditor" = "none";
-      "workbench.iconTheme" = "catppuccin-mocha";
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "catppuccin.accentColor" = "pink";
-      "terminal.external.osxExec" = "Alacritty.app";
-      "terminal.integrated.shell.osx" = "/bin/zsh";
-      "terminal.integrated.fontFamily" = "MesloLGS NF";
-      "terminal.integrated.fontSize" = 16;
     };
   };
 }
