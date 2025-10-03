2d79732113f60dc6ad3ed97e75b83f20e1cb22c8
pbthapa <ptpbthapa24@gmail.com>
2025-10-03 08:34:05 +0200

    update deprecrated fields

--- home-manager/apps/kitty.nix
+++ home-manager/apps/kitty.nix
@@ -2,6 +2,6 @@
   programs.kitty = {
     enable = true;
     extraConfig = builtins.readFile ./config/kitty/kitty.conf;
-    theme = "Dracula";
+    themeFile = pkgs.kitty-themes."Dracula"; 
   };
 }
